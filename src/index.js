1d27d8c3e3969b9ee824703c09bc773234a8bf5c
Anubic29 <asitailo2039@gmail.com>
2022-01-17 21:47:23 +0200

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  closes = '';
+  str.split('').some(sym => {
+    const pos = bracketsConfig.find((arr, index) => {
+      if (arr.includes(sym)) return arr;
+    })
+    if (pos !== undefined) {
+      if (sym === pos[0]) {
+        if (sym === pos[1]) {
+          if (closes.startsWith(sym)) {
+            closes = closes.substring(1);
+          } else {
+            closes = `${pos[1]}${closes}`;
+          }
+        } else {
+          closes = `${pos[1]}${closes}`;
+        }
+      } else {
+        if (closes.startsWith(sym)) {
+          closes = closes.substring(1);
+        } else {
+          closes = `${pos[1]}${closes}`;
+        }
+      }
+    }
+  });
+  if (closes === '') return true;
+  return false;
 }
